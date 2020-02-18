b02a9b7a59016155bfdd8969ecbcb84001384004
daniellesuper <danielle.super7@gmail.com>
2020-02-18 09:30:29 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { css } from "@emotion/core";
+
+import ClipLoader from "react-spinners/ClipLoader";
+
+
 
 import Toolbar from './components/Toolbar/Toolbar';
 import SideDrawer from './components/SideDrawer/SideDrawer';
 import Backdrop from './components/Backdrop/Backdrop';
 import Footer from './components/Footer/Footer';
 import Slideshow from './components/Slideshow/Slideshow';
+import Loader from './components/Loader/Loader';
 
 
 
