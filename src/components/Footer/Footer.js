c452954e7da5468d1ad8349ee1db75d0b51ab9c9
daniellesuper <danielle.super7@gmail.com>
2020-02-20 13:19:24 -0500

    updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,11 +5,11 @@ class Footer extends React.Component{
   render(){
     return(
       <footer className="footer">
-        Elevation Software<br/>
-        (678) 923 - 7694<br/>
-        <a>info@elevationsoftware.io</a>
+        ELEVATION SOFTWARE<br/>
+        <b>(678) 923-7694</b><br/>
+        <a>INFO@ELEVATIONSOFTWARE.IO</a>
         <div id="bar"></div>
-        <div id="copy">COPYRIGHT 2020, ELEVATION SOFTWARE SERVICE INC.</div>
+        <div id="copy">COPYRIGHT © 2020<br /> ELEVATION SOFTWARE SERVICE INC.</div>
       </footer>
     );
   }
